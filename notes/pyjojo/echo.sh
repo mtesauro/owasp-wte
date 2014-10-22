a440609399ba13abed555fac75f410c55a447e1b
Matt Tesauro <mtesauro@gmail.com>
2014-10-22 03:10:31 +0000

    pyjojo experiment

---
+++ notes/pyjojo/echo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# -- jojo --
+# description: echo text on the command line
+# param: text - text to echo
+# -- jojo --
+
+echo "echo'd text: $TEXT"
+echo "jojo_return_value name=bob"
+echo "jojo_return_value age=99"
+exit 0
